ca2f44d63f7d70f945fdde8ac5f2f33ab1841d3e
tditada <tditada@gmail.com>
2022-07-11 16:25:39 +0200

    getting only 3 upcoming months launches

--- src/components/Launches.tsx
+++ src/components/Launches.tsx
@@ -1,9 +1,19 @@
 import axios from 'axios';
+import moment from 'moment';
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { Launch } from './types';
 
 const LAUNCHES_API = 'https://lldev.thespacedevs.com/2.2.0/launch/';
-const LIMIT_API = 50;
+const LIMIT_API = 500; //TODO: Set pagination
+
+const UPCOMING_TEXT = 'Upcoming launches next 3 months';
+const NAME = 'Name:';
+const LAUNCH = 'Launch:';
+
+const StyledLaunchItem = styled.div`
+  margin: 0.5em;
+`;
 
 const Launches = () => {
     const [launchesList, setLaunchesList] = useState<Array<Launch>>([]);
@@ -11,10 +21,21 @@ const Launches = () => {
     const [error, setError] = useState<string | null>(null);
     const [offset, setOffset] = useState<number>(0);
 
+    const now = moment.utc().format();
+    const three_months = moment.utc().add(3, 'months').format(); // Moment 'months': from 11/7 to 11/10 there's 3 month.
+
     const getData = async () => {
         try {
-            const response = await axios.get(LAUNCHES_API, { params: { limit: LIMIT_API}});
-            console.log(response.data);
+            const response = await axios.get(
+                LAUNCHES_API, 
+                { 
+                    params: { 
+                        limit: LIMIT_API, 
+                        net__gt: now, 
+                        net__lt: three_months 
+                    } 
+                }
+            );
             setLaunchesList(response.data.results);
             setOffset(offset + LIMIT_API);
             setError(null);
@@ -32,8 +53,12 @@ const Launches = () => {
 
     return (
         <div>
+            <div>{UPCOMING_TEXT}</div>
             {launchesList.map((launch) => {
-                return <div>{launch.name} | {launch.net}</div>
+                return <StyledLaunchItem key={launch.id}>
+                    <div>{NAME} {launch.name} </div>
+                    <div>{LAUNCH} {launch.net}</div>
+                </StyledLaunchItem>
             })}
         </div>
     );
--- src/components/types.ts
+++ src/components/types.ts
@@ -1,4 +1,5 @@
 export type Launch = {
+    id: string,
     name: string,
     net: string,
 };
